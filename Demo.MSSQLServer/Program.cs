cce9b0f5894db71ff6828a91fe1d9accdbd5b773
yzk <yzk365@qq.com>
2021-06-04 10:48:14 +0800

    BulkInsertAsync of mssqlserver

--- Demo.MSSQLServer/Program.cs
+++ Demo.MSSQLServer/Program.cs
@@ -1,4 +1,7 @@
 ﻿using Demo.Base;
+using System;
+using System.Linq;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Demo
@@ -10,8 +13,16 @@ namespace Demo
         {
             using (TestDbContext ctx = new TestDbContext())
             {
-                 await TestCase1.RunAsync(ctx);
-                 await TestCase2.RunAsync(ctx);       
+                //await TestCase1.RunAsync(ctx);
+                // await TestCase2.RunAsync(ctx);
+                List<Book> books = new List<Book>();
+                for(int i=0;i<100;i++)
+                {
+                    books.Add(new Book { AuthorName = "abc"+i, Price = new Random().NextDouble(), PubTime = DateTime.Now, Title = Guid.NewGuid().ToString() });
+                }
+                await ctx.BulkInsertAsync(books);
+                // await ctx.BulkInsertAsync(new List<Comment>());
+               // await ctx.BulkInsertAsync(new List<Article>());
             }
         }
     }
