5d4261cb910e943222dea819ba6bac2cc2f6b9a0
胡兴业 <361694192@qq.com>
2023-11-17 11:35:37 +0800

    配置默认值  无法插入数据的Bug (#121)

--- Demo.Oracle/Program.cs
+++ Demo.Oracle/Program.cs
@@ -14,7 +14,10 @@ namespace Demo
 			{
 				var list = new List<Book>();
 
-				list.Add(new Book { Title = "123" });
+				list.Add(new Book { Title = "1", Price = new Random().Next(0, 100), PubTime = DateTime.Now, AuthorName = "v", Pages = 2 });
+				list.Add(new Book { Title = "2", Price = new Random().Next(0, 100), PubTime = DateTime.Now, AuthorName = "v", Pages = 2 });
+				list.Add(new Book { Title = "3", Price = new Random().Next(0, 100), PubTime = DateTime.Now, AuthorName = "v", Pages = 2 });
+				list.Add(new Book { Title = "4", Price = new Random().Next(0, 100), PubTime = DateTime.Now, AuthorName = "v", Pages = 2 });
 
 				await ctx.BulkInsertAsync(list, OracleBulkCopyOptions.UseInternalTransaction);
 			}
